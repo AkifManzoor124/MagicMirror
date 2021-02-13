b0dd0d645f9b7d83f86eeb2c9c8a0408900d8023
Akif Manzoor <akifmanzoor124@gmail.com>
2021-02-13 11:31:25 -0500

    added onscreenmenu extension

--- config/config.js
+++ config/config.js
@@ -10,18 +10,18 @@
 
 var config = {
 	address: "localhost", 	// Address to listen on, can be:
-							// - "localhost", "127.0.0.1", "::1" to listen on loopback interface
-							// - another specific IPv4/6 to listen on a specific interface
-							// - "0.0.0.0", "::" to listen on any interface
-							// Default, when address config is left out or empty, is "localhost"
+	// - "localhost", "127.0.0.1", "::1" to listen on loopback interface
+	// - another specific IPv4/6 to listen on a specific interface
+	// - "0.0.0.0", "::" to listen on any interface
+	// Default, when address config is left out or empty, is "localhost"
 	port: 8080,
 	basePath: "/", 	// The URL path where MagicMirror is hosted. If you are using a Reverse proxy
-					// you must set the sub path here. basePath must end with a /
+	// you must set the sub path here. basePath must end with a /
 	ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1"], 	// Set [] to allow all IP addresses
-															// or add a specific IPv4 of 192.168.1.5 :
-															// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.1.5"],
-															// or IPv4 range of 192.168.3.0 --> 192.168.3.15 use CIDR format :
-															// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.3.0/28"],
+	// or add a specific IPv4 of 192.168.1.5 :
+	// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.1.5"],
+	// or IPv4 range of 192.168.3.0 --> 192.168.3.15 use CIDR format :
+	// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.3.0/28"],
 
 	useHttps: false, 		// Support HTTPS or not, default "false" will use HTTP
 	httpsPrivateKey: "", 	// HTTPS private key path, only require when useHttps is true
@@ -74,7 +74,7 @@ var config = {
 					}
 				]
 			}
-		},		
+		},
 		{
 			module: "compliments",
 			position: "lower_third"
@@ -121,8 +121,24 @@ var config = {
 				apiKey: "D7N5BWM5S3YI36DSKMY5ULEY2GIR6QTW"
 			}
 		},
+		{
+			module: 'MMM-OnScreenMenu',
+			position: 'bottom_right',
+			/* Valid positions: 'top_right', 'top_left', 'bottom_right', 'bottom_left' */
+			config: {
+				touchMode: true,
+				menuItems: {
+					monitorOff: { title: "Turn Off Monitor", icon: "television", source: "SERVER" },
+					restart: { title: "Restart MagicMirror", icon: "recycle", source: "ALL" },
+					refresh: { title: "Refresh MagicMirror", icon: "refresh", source: "LOCAL" },
+					reboot: { title: "Reboot", icon: "spinner", source: "ALL" },
+					shutdown: { title: "Shutdown", icon: "power-off", source: "ALL" },
+				},
+				enableKeyboard: true,
+			}
+		},
 	]
 };
 
 /*************** DO NOT EDIT THE LINE BELOW ***************/
-if (typeof module !== "undefined") {module.exports = config;}
+if (typeof module !== "undefined") { module.exports = config; }
